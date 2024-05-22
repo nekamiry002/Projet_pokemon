778971c8e6cd6b0d96c648a5207ee7caa9f0249b
nekamiry002 <115090705+nekamiry002@users.noreply.github.com>
2024-05-22 14:14:08 +0200

    Update get.js

--- controler/Pokemon/get.js
+++ controler/Pokemon/get.js
@@ -1,17 +1,5 @@
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-
-const urlMongoDb = process.env.URL_MONGODB ;
-const app = express();
-
-const express = require('express')
-const fs = require('fs/promises');
-const fileData = require('../data/pokemon-data.json')
-const axios = require('axios');
-const { type } = require('os');
-const cors = require('cors');
 // import initDatabase from './database.js';
-const initDatabase = require('../database');
+const initDatabase = require('../../database');
 
 async function getPokemonById( id ){
     const connectedDb = await initDatabase();
